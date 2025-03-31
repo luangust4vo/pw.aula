fe43a5cbac930c0383c2429d5748daee012a8ce8
Luan Gustavo <gluan1213@gmail.com>
2025-03-31 20:48:42 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    searchSubs();
+});
+
+function changeTheme() {
+    let icon_theme = document.getElementById('toggle-button');
+
+    let body = document.querySelector('body');
+    let theme = body.getAttribute('data-theme');
+    if (theme === 'light') {
+        body.setAttribute('data-theme', '');
+        icon_theme.src = "assets/sun.png"
+    } else {
+        body.setAttribute('data-theme', 'light');
+        icon_theme.src = "assets/moon.png"
+    }
+}
+
+function learnMore() {
+    const modal = document.querySelector('.modal');
+
+    modal.classList.remove('hidden');
+    modal.addEventListener('click', e => {
+        if (e.target == modal) {
+            modal.classList.add('hidden');
+        }
+    });
+}
+
+async function searchSubs() {
+    fetch("json/subs.json").then(res => res.json()).then(res => {
+        const subDiv = document.getElementById('subs-list');
+        
+        res.forEach(e => {
+            let p = document.createElement("p");
+            p.textContent = `Nome: ${e.nome}`;
+            subDiv.appendChild(p);
+        });
+    })
+}
