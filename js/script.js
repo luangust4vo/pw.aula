1d3994b9d39e4499fbfebe22d7bec94bd1f05b77
Luan Gustavo <gluan1213@gmail.com>
2025-04-07 20:49:29 -0300

    Add change language feature and acessibility stuff

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+let currentLanguage = 'pt-br';
+let modal;
+
 document.addEventListener("DOMContentLoaded", (event) => {
     searchSubs();
-});
 
-let modal;
+    if (localStorage.getItem('language')) {
+        currentLanguage = localStorage.getItem('language');
+        loadLanguage(currentLanguage);
+    }
+});
 
 function changeTheme() {
     let icon_theme = document.getElementById('toggle-button');
@@ -43,4 +49,31 @@ async function searchSubs() {
             subDiv.appendChild(p);
         });
     })
+}
+
+function changeLanguage() {
+    currentLanguage = currentLanguage === 'pt-br' ? 'en' : 'pt-br';
+    loadLanguage(currentLanguage);
+
+    localStorage.setItem('language', currentLanguage);
+}
+
+function loadLanguage(language) {
+    fetch(`../json/language/${language}.json`).then(res => res.json()).then(res => {
+        translatePage(res);
+    });
+}
+
+function translatePage(language) {
+    document.querySelectorAll('[data-i18n]').forEach(e => {
+        let item = e.getAttribute('data-i18n');
+        
+        e.textContent = language[item];
+    });
+
+    document.querySelectorAll('[data-i18n-alt]').forEach(e => {
+        let item = e.getAttribute('data-i18n-alt');
+
+        e.setAttribute('alt', language.images[item]);
+    });
 }
